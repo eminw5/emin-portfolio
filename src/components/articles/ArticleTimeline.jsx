1d009dc280d02150583f2ed78043d02c4079cd74
emin <jcusoft.dev@gmail.com>
2025-04-18 10:32:13 -0400

    ui update

--- src/components/articles/ArticleTimeline.jsx
+++ src/components/articles/ArticleTimeline.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Article from "/src/components/wrappers/Article.jsx"
-import Timeline from "/src/components/generic/Timeline.jsx"
-import {useParser} from "/src/helpers/parser.js"
+import React from "react";
+import Article from "/src/components/wrappers/Article.jsx";
+import Timeline from "/src/components/generic/Timeline.jsx";
+import { useParser } from "/src/helpers/parser.js";
 
 function ArticleTimeline({ data }) {
-    const parser = useParser()
+  const parser = useParser();
 
-    const parsedData = parser.parseArticleData(data)
-    const items = parsedData.items
-    parser.sortArticleItemsByDateDesc(items)
+  const parsedData = parser.parseArticleData(data);
+  const items = parsedData.items;
+  // parser.sortArticleItemsByDateDesc(items)
 
-    const parsedItems = parser.formatForTimeline(items)
+  const parsedItems = parser.formatForTimeline(items);
 
-    return (
-        <Article className={`article-timeline`} title={ parsedData.title }>
-            <Timeline items={parsedItems}/>
-        </Article>
-    )
+  return (
+    <Article className={`article-timeline`} title={parsedData.title}>
+      <Timeline items={parsedItems} />
+    </Article>
+  );
 }
 
-export default ArticleTimeline
+export default ArticleTimeline;
